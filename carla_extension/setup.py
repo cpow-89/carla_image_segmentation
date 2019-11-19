d5b1f557d158c2e703533c8d43225babd698758a
Christoph Powazny <dnw_mailorder@gmx.de>
2019-11-19 20:54:35 +0100

    add function for getting unique random spawn points for a given world

--- carla_extension/setup.py
+++ carla_extension/setup.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+import random
 
 
 def set_system_path_to_carla_egg(relative_path):
@@ -12,3 +13,14 @@ def set_system_path_to_carla_egg(relative_path):
         sys.path.append(glob.glob(os.path.join(relative_path, file_name))[0])
     except IndexError:
         pass
+
+
+def get_n_unique_spawn_points(world, n):
+    """Sample a list of n unique spawn points for the given world"""
+    max_number_samples = len(world.get_map().get_spawn_points())
+    if n <= max_number_samples:
+        return random.sample(world.get_map().get_spawn_points(), k=n)
+    else:
+        print("Number of requested spawn points is to high.")
+        print("n has been set to the max value, which is {}.".format(max_number_samples))
+        return random.sample(world.get_map().get_spawn_points(), k=max_number_samples)
