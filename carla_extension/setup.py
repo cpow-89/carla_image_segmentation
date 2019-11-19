8e980527809cbb2faf1fbbc4c48f9d01a5cdd343
Christoph Powazny <dnw_mailorder@gmx.de>
2019-11-19 22:19:00 +0100

    delete function for setting up system path. This function is not needed if we simply easy_install the carla python egg.
    add function for creating ego vehicle camera sensors based on config file

--- carla_extension/setup.py
+++ carla_extension/setup.py
@@ -1,18 +1,5 @@
-import glob
-import os
-import sys
 import random
-
-
-def set_system_path_to_carla_egg(relative_path):
-    try:
-        file_name = "carla-*%d.%d-%s.egg" % (
-            sys.version_info.major,
-            sys.version_info.minor,
-            'win-amd64' if os.name == 'nt' else 'linux-x86_64')
-        sys.path.append(glob.glob(os.path.join(relative_path, file_name))[0])
-    except IndexError:
-        pass
+import carla
 
 
 def get_n_unique_spawn_points(world, n):
@@ -24,3 +11,17 @@ def get_n_unique_spawn_points(world, n):
         print("Number of requested spawn points is to high.")
         print("n has been set to the max value, which is {}.".format(max_number_samples))
         return random.sample(world.get_map().get_spawn_points(), k=max_number_samples)
+
+
+def create_ego_vehicle_camera_sensors(config_camera_sensors, blueprint_library):
+    ego_camera_sensors = []
+    for camera_sensor_name, camera_sensor_values in config_camera_sensors.items():
+        camera_blueprint = blueprint_library.find("sensor.camera.{}".format(camera_sensor_name))
+        for attr_name, attr_value in camera_sensor_values["attributes"].items():
+            camera_blueprint.set_attribute(attr_name, attr_value)
+        ego_camera_sensors.append(camera_blueprint)
+        camera_transform = carla.Transform(carla.Location(x=camera_sensor_values["location"]["x"],
+                                                          y=camera_sensor_values["location"]["y"],
+                                                          z=camera_sensor_values["location"]["x"]))
+        ego_camera_sensors.append((camera_blueprint, camera_transform))
+    return ego_camera_sensors
