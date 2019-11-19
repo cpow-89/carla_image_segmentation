ba11cc33325ed6f24b88a1d54567f81b91f6ec29
Christoph Powazny <dnw_mailorder@gmx.de>
2019-11-19 22:20:58 +0100

    update file to use new spawn and create sensor functions

--- collect_train_data.py
+++ collect_train_data.py
@@ -1,15 +1,22 @@
+import json
 import os
+import carla
 import carla_extension.setup as carla_setup
 
 
-carla_setup.set_system_path_to_carla_egg(os.path.join("..", "..", "carla", "PythonAPI", "carla", "dist"))
-
-import carla
-
-
 def main():
+    with open(os.path.join(".", "configs", "camera_sensors.json"), "r") as read_file:
+        config_camera_sensors = json.load(read_file)
+
     client = carla.Client("localhost", 2000)
     client.set_timeout(10.0)
+
+    world = client.get_world()
+    blueprint_library = world.get_blueprint_library()
+    spawn_points = carla_setup.get_n_unique_spawn_points(world, n=1)
+
+    ego_vehicle_camera_sensors = carla_setup.create_ego_vehicle_camera_sensors(config_camera_sensors, blueprint_library)
+
     print("Done")
 
 
