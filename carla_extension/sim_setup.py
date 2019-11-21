593db074404370dd8d90c43ce2207eb568676f23
Christoph Powazny <dnw_mailorder@gmx.de>
2019-11-21 19:30:08 +0100

    Refactoring sim setup. Change create_ego_vehicle_camera_sensors function to only creating a blueprint and setting the basic camera attributes.

---
+++ carla_extension/sim_setup.py
@@ -0,0 +1,18 @@
+import random
+
+
+def get_n_unique_spawn_points(world, n):
+    """Sample a list of n unique spawn points for the given world"""
+    max_number_samples = len(world.get_map().get_spawn_points())
+    if n <= max_number_samples:
+        return random.sample(world.get_map().get_spawn_points(), k=n)
+    else:
+        print("Number of requested spawn points is to high.")
+        print("n has been set to the max value, which is {}.".format(max_number_samples))
+        return random.sample(world.get_map().get_spawn_points(), k=max_number_samples)
+
+
+def create_basic_camera_blueprint(blueprint_library, camera_type, attributes):
+    camera_blueprint = blueprint_library.find("sensor.camera.{}".format(camera_type))
+    for name, value in attributes.items():
+        camera_blueprint.set_attribute(name, value)
