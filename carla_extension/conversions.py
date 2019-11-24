215ec0bfe7a95e48c168ce3c71cce7931b210fe6
Christoph Powazny <dnw_mailorder@gmx.de>
2019-11-24 20:27:46 +0100

    Add image_seg to label and label to image_seg conversions

--- carla_extension/conversions.py
+++ carla_extension/conversions.py
@@ -17,3 +17,36 @@ def convert_carla_brga_image_stream_to_seg_mask(carla_bgra_image):
     """Convert a carla brga image stream to a numpy 2d image segmentation mask"""
     brga_array = convert_carla_bgra_image_stream_to_brga_array(carla_bgra_image)
     return convert_brga_array_to_seg_mask(brga_array)
+
+
+image_segmentation_mask_to_labels = {
+    0: "Unlabeled",
+    1: "Building",
+    2: "Fence",
+    3: "Other",
+    4: "Pedestrian",
+    5: "Pole",
+    6: "Road line",
+    7: "Road",
+    8: "Sidewalk",
+    9: "Vegetation",
+    10: "Car",
+    11: "Wall",
+    12: "Traffic sign"
+}
+
+labels_to_image_segmentation_mask = {
+    "Unlabeled": 0,
+    "Building": 1,
+    "Fence": 2,
+    "Other": 3,
+    "Pedestrian": 4,
+    "Pole": 5,
+    "Road line": 6,
+    "Road": 7,
+    "Sidewalk": 8,
+    "Vegetation": 9,
+    "Car": 10,
+    "Wall": 11,
+    "Traffic sign": 12
+}
